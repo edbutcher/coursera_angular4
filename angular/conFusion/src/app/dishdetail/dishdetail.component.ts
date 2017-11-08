290041592653b82591eec2093fb06f415d69c36b
edbutcher <edbutcher200@gmail.com>
2017-11-08 18:06:51 +0200

    Animations Part 2

--- angular/conFusion/src/app/dishdetail/dishdetail.component.ts
+++ angular/conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-import { trigger, state, style, animate, transition } from '@angular/animations';
 
 import { Dish } from '../shared/dish';
 import { Comment } from '../shared/comment';
 import { DishService } from '../services/dish.service';
+import { visibility, flyInOut, expand } from '../animations/app.animation';
 
 import 'rxjs/add/operator/switchMap';
 
@@ -14,18 +14,14 @@ import 'rxjs/add/operator/switchMap';
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss'],
+  host: {
+  '[@flyInOut]': 'true',
+  'style': 'display: block;'
+  },
   animations: [
-    trigger('visibility', [
-        state('shown', style({
-            transform: 'scale(1.0)',
-            opacity: 1
-        })),
-        state('hidden', style({
-            transform: 'scale(0.5)',
-            opacity: 0
-        })),
-        transition('* => *', animate('0.1s ease-in-out'))
-    ])
+    visibility(),
+    flyInOut(),
+    expand()
   ]
 })
 
